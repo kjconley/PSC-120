e2349206af5096504a1ee529d91b43e89e287c45
Kevin Conley <kjconley@ucdavis.edu>
2018-10-08 16:47:55 -0700

    fix imports temp

--- College People/src/Program/CollegePeople.java
+++ College People/src/Program/CollegePeople.java
@@ -1,7 +1,14 @@
 package Program;
 
+import Students.Undergrad;
+import academics.Course;
+
 public class CollegePeople {
 	public void run() {
+		Course c = new Course();
+		c.addStudent(new Undergrad());
+		c.addStudent(new Undergrad());
+		c.getNextStudent().addGrade(100);
 	}
 	
 	public static void main(String[] args) {
